bae9826633f4c6a4cd07887e8317202f0ea05124
Mejanur Rahman Akash <mrmezan06@gmail.com>
2021-11-04 16:40:56 +0600

    Dart types and assigning types to variables

--- Hello.dart
+++ Hello.dart
@@ -1,3 +1,14 @@
-main(List<String> arguments){
-  print('Hello world');
+main(List<String> arguments){ //entry point for execution
+  var opponent =  "Pakistan";
+  /*
+  * String
+  * Number(integer and double
+  * Boolean
+  * list
+  * map
+  */
+  int year = 1971;
+  String country =  "Bangladesh";
+
+  print('The $country Liberation War in $year was for independence from $opponent.');
 }
