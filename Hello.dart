597b1a4b358232d50714322e46fbb5fd2316265f
Mejanur Rahman Akash <mrmezan06@gmail.com>
2021-11-04 17:16:42 +0600

    const and final

--- Hello.dart
+++ Hello.dart
@@ -1,14 +1,8 @@
 main(List<String> arguments){ //entry point for execution
-  var opponent =  "Pakistan";
-  /*
-  * String
-  * Number(integer and double
-  * Boolean
-  * list
-  * map
-  */
-  int year = 1971;
-  String country =  "Bangladesh";
-
-  print('The $country Liberation War in $year was for independence from $opponent.');
+ const a = 10;
+ final b = 20;
+ print(a);
+ //a = 20; const not changeable compile time const
+ print(b);
+ //b = 30; final not changeable all the time const
 }
